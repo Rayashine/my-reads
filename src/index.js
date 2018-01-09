1582454983560ad5b2a656077368efb98c54ea9e
闷头驴 <qianxuejia@itjuzi.com>
2018-01-09 19:54:50 +0800

    Some test.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <BrowserRouter><App /></BrowserRouter>,
         document.getElementById('root'));
-registerServiceWorker();
